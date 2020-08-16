0098098221ba3a3c0f2cecb05f7ac0fe4666d4f8
balan <balan.pmr@outlook.com>
2020-08-16 00:39:46 -0500

    Updates

---
+++ precache-manifest.dae9959982d1cd1e004aca3dfaf75514.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a190e201eb342bc0fe83f7f9f605af5",
+    "url": "/gym/index.html"
+  },
+  {
+    "revision": "8f77dc5c6c497ce19a43",
+    "url": "/gym/static/css/2.10f67ca9.chunk.css"
+  },
+  {
+    "revision": "8f71d86185bb29d4cd2a",
+    "url": "/gym/static/css/main.8c390389.chunk.css"
+  },
+  {
+    "revision": "8f77dc5c6c497ce19a43",
+    "url": "/gym/static/js/2.b4f67961.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/gym/static/js/2.b4f67961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f71d86185bb29d4cd2a",
+    "url": "/gym/static/js/main.bf91c462.chunk.js"
+  },
+  {
+    "revision": "4e97f9fac717eb225510",
+    "url": "/gym/static/js/runtime-main.547a0567.js"
+  }
+]);
