0c57965d257638b638a595623947df94c88c1e05
balan <balan.pmr@outlook.com>
2020-09-15 09:04:01 -0500

    Updates

--- precache-manifest.66f240fd2135dc73975f16e3e3b5dea3.js
+++ precache-manifest.66f240fd2135dc73975f16e3e3b5dea3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3ed3fceda2eed3056a6ab20e0539cd10",
+    "revision": "f98127a6e6386f781fce938b9c62be69",
     "url": "/gym/index.html"
   },
   {
-    "revision": "242b017f10e6ff93cede",
+    "revision": "e9efaa120dadf38f9d3e",
     "url": "/gym/static/css/2.10f67ca9.chunk.css"
   },
   {
-    "revision": "3ac3ea6b81986641f230",
+    "revision": "39cbd383027f67fb497c",
     "url": "/gym/static/css/main.a9927d5b.chunk.css"
   },
   {
-    "revision": "242b017f10e6ff93cede",
-    "url": "/gym/static/js/2.092ce271.chunk.js"
+    "revision": "e9efaa120dadf38f9d3e",
+    "url": "/gym/static/js/2.c48ed0ce.chunk.js"
   },
   {
     "revision": "0b08544f54c9906705be39c03b7e79ad",
-    "url": "/gym/static/js/2.092ce271.chunk.js.LICENSE.txt"
+    "url": "/gym/static/js/2.c48ed0ce.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3ac3ea6b81986641f230",
-    "url": "/gym/static/js/main.ee957466.chunk.js"
+    "revision": "39cbd383027f67fb497c",
+    "url": "/gym/static/js/main.7e4e3f96.chunk.js"
   },
   {
     "revision": "4e97f9fac717eb225510",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gym/precache-manifest.c0225997230b539d668115ed938a9397.js"
+  "/gym/precache-manifest.66f240fd2135dc73975f16e3e3b5dea3.js"
 );
 
 self.addEventListener('message', (event) => {
